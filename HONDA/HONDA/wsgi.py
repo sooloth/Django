51a2563fe1f8cf8a6387e38af0f66cc42034b9f1
sooloth <pangnuar@gmail.com>
2024-01-16 09:51:03 +0700

    first

---
+++ HONDA/HONDA/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for HONDA project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HONDA.settings')
+
+application = get_wsgi_application()
---
+++ HONDA/HONDAAFEI/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HondaafeiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HONDAAFEI'
